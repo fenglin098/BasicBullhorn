2396d61e5582c466d83406f5b280cb2066fb7ac4
fenglin098 <feng.lin098@gmail.com>
2019-11-20 15:12:56 -0500

    update works without uploading new pictures

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,20 +1,26 @@
 package com.example.demo;
 
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class HomeController {
+
     @Autowired
     MessageRepository messageRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
 
     @RequestMapping("/")
     public String listMessages(Model model){
@@ -29,12 +35,24 @@ public class HomeController {
     }
 
     @PostMapping("/process")
-    public String processForm(@Valid Message message, BindingResult result){
+    public String processForm(@Valid Message message, BindingResult result, @RequestParam("file")MultipartFile file){
         if (result.hasErrors()){
             return "messageform";
         }
-        messageRepository.save(message);
-        return "redirect:/";
+        if (file.isEmpty()){
+            messageRepository.save(message);
+            return "redirect:/";
+        }
+        try {
+            Map uploadResult = cloudc.upload(file.getBytes(),
+                    ObjectUtils.asMap("resourcetype", "auto"));
+            message.setPic(uploadResult.get("url").toString());
+            messageRepository.save(message);
+        }catch (IOException e){
+            e.printStackTrace();
+            return "redirect:/add";
+        }return "redirect:/";
+
     }
 
     @RequestMapping("/detail/{id}")
@@ -55,5 +73,10 @@ public class HomeController {
         return "redirect:/";
     }
 
+    @PostMapping("/searchlist")
+    public String search(Model model, @RequestParam("search") String search){
+        model.addAttribute("messages", messageRepository.findByContentContainingIgnoreCaseOrTitleContainingIgnoreCase(search, search));
+        return "searchlist";
+    }
 
 }
--- src/main/java/com/example/demo/MessageRepository.java
+++ src/main/java/com/example/demo/MessageRepository.java
@@ -2,5 +2,10 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface MessageRepository extends CrudRepository <Message, Long> {
+    ArrayList<Message> findByContentContainingIgnoreCaseOrTitleContainingIgnoreCase(String content, String title);
+
 }
+
